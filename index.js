c40b263210ccc89cc0fc8d666165757d4820a667
Amariah <amariah.abish@gmail.com>
2024-03-19 07:56:17 +0300

    Phase 1 JS Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage using concatenation
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+// Define excitedWelcomeMessage using .toUpperCase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define shortGreeting with first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
